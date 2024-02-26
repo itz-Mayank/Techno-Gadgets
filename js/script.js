be80329bef8b92c9baee418b03bb09d09f5d22af
Mayank Meghwal <mayankmeg207@gmail.com>
2024-02-26 08:20:05 +0530

    files

---
+++ js/script.js
@@ -0,0 +1,221 @@
+(function($) {
+  // JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector('.slider');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+
+  function nextSlide() {
+    slider.style.transform = 'translateX(-100%)';
+    setTimeout(() => {
+      slider.appendChild(slider.firstElementChild);
+      slider.style.transform = 'translateX(0)';
+    }, 500);
+  }
+
+  function prevSlide() {
+    slider.style.transition = 'none';
+    slider.style.transform = 'translateX(-100%)';
+    slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+    setTimeout(() => {
+      slider.style.transition = 'transform 0.5s ease-in-out';
+      slider.style.transform = 'translateX(0)';
+    }, 0);
+  }
+
+  nextBtn.addEventListener('click', nextSlide);
+  prevBtn.addEventListener('click', prevSlide);
+
+  setInterval(nextSlide, 3000);
+});
+
+
+  "use strict";
+
+  var initPreloader = function() {
+    $(document).ready(function($) {
+    var Body = $('body');
+        Body.addClass('preloader-site');
+    });
+    $(window).load(function() {
+        $('.preloader-wrapper').fadeOut();
+        $('body').removeClass('preloader-site');
+    });
+  }
+
+  var swiper = new Swiper(".testimonial-swiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-arrow-next",
+      prevEl: ".swiper-arrow-prev",
+    },
+  }); 
+
+  // init Chocolat light box
+	var initChocolat = function() {
+		Chocolat(document.querySelectorAll('.image-link'), {
+		  imageSize: 'contain',
+		  loop: true,
+		})
+	}
+
+  var initSwiper = function() {
+
+    var swiper = new Swiper(".main-swiper", {
+      speed: 500,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    var category_swiper = new Swiper(".category-carousel", {
+      slidesPerView: 6,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".category-carousel-next",
+        prevEl: ".category-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1500: {
+          slidesPerView: 6,
+        },
+      }
+    });
+
+    var brand_swiper = new Swiper(".brand-carousel", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".brand-carousel-next",
+        prevEl: ".brand-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+        1500: {
+          slidesPerView: 4,
+        },
+      }
+    });
+
+    var products_swiper = new Swiper(".products-carousel", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".products-carousel-next",
+        prevEl: ".products-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1500: {
+          slidesPerView: 6,
+        },
+      }
+    });
+
+    // product single page
+    var thumb_slider = new Swiper(".product-thumbnail-slider", {
+      slidesPerView: 5,
+      spaceBetween: 20,
+      // autoplay: true,
+      direction: "vertical",
+      breakpoints: {
+        0: {
+          direction: "horizontal"
+        },
+        992: {
+          direction: "vertical"
+        },
+      },
+    });
+
+    var large_slider = new Swiper(".product-large-slider", {
+      slidesPerView: 1,
+      // autoplay: true,
+      spaceBetween: 0,
+      effect: 'fade',
+      thumbs: {
+        swiper: thumb_slider,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+
+  // input spinner
+  var initProductQty = function(){
+
+    $('.product-qty').each(function(){
+
+      var $el_product = $(this);
+      var quantity = 0;
+
+      $el_product.find('.quantity-right-plus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          $el_product.find('#quantity').val(quantity + 1);
+      });
+
+      $el_product.find('.quantity-left-minus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          if(quantity>0){
+            $el_product.find('#quantity').val(quantity - 1);
+          }
+      });
+
+    });
+
+  }
+
+  // init jarallax parallax
+  var initJarallax = function() {
+    jarallax(document.querySelectorAll(".jarallax"));
+
+    jarallax(document.querySelectorAll(".jarallax-keep-img"), {
+      keepImg: true,
+    });
+  }
+
+  // document ready
+  $(document).ready(function() {
+    
+    initPreloader();
+    initSwiper();
+    initProductQty();
+    initJarallax();
+    initChocolat();
+
+  }); // End of a document
+
+})(jQuery);
